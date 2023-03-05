5c48380d59dbcadad3fc481145fde18453ccf22d
kamilzyczynski <kamil.zyczynski@gmail.com>
2023-03-05 12:47:28 +0100

    Poprawa kodu.

--- src/main/java/pl/javastart/task/Main.java
+++ src/main/java/pl/javastart/task/Main.java
@@ -4,7 +4,6 @@ public class Main {
     public static void main(String[] args) {
         Monitor monitor = new Monitor("LG", 21, 144);
 
-
         System.out.println("Informacje o monitorze:");
         System.out.println("Producent: " + monitor.producer);
         System.out.println("Rozmiar ekranu [cale]: " + monitor.size);
